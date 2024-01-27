b412ad2d4e3088274d6d4a23350f69ddf89b8fbf
alexakodyka <153808789+alexakodyka@users.noreply.github.com>
2024-01-27 13:53:09 +0500

    Create default.nix

---
+++ hosts/acer/default.nix
@@ -0,0 +1,10 @@
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
+  # Enable my Lenovo Keyboard
+  #boot.kernelParams = [ "i8042.direct" "i8042.dumbkbd" "i915.force_probe=9a49" ];
+  # ---- System Configurations ----
+  #powerManagement.powertop.enable = true;
+  #services.thermald.enable = true;
+}
