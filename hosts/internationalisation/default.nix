571b972d10c9df6908f3dd4dded772fc95ebe338
alexakodyka <153808789+alexakodyka@users.noreply.github.com>
2024-01-27 14:54:51 +0500

    Update default.nix

--- hosts/internationalisation/default.nix
+++ hosts/internationalisation/default.nix
@@ -1,9 +1,21 @@
 {
   # Set your time zone.
-  time.timeZone = "America/Toronto";
+  time.timeZone = "Asia/Yekaterinburg";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_CA.UTF-8";
+  i18n.defaultLocale = "ru_RU.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "ru_RU.UTF-8";
+    LC_IDENTIFICATION = "ru_RU.UTF-8";
+    LC_MEASUREMENT = "ru_RU.UTF-8";
+    LC_MONETARY = "ru_RU.UTF-8";
+    LC_NAME = "ru_RU.UTF-8";
+    LC_NUMERIC = "ru_RU.UTF-8";
+    LC_PAPER = "ru_RU.UTF-8";
+    LC_TELEPHONE = "ru_RU.UTF-8";
+    LC_TIME = "ru_RU.UTF-8";
+  };
 
   services.xserver = {
     layout = "us";
